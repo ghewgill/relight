ed461be7fa1ba4e3b2240c61b50c531014bbf64f
Greg Hewgill <greg@hewgill.com>
2009-11-20 18:29:00 +1300

    initial commit

---
+++ relight.py
@@ -0,0 +1,35 @@
+import os
+import sys
+import time
+
+current_minute = time.time() / 60
+restarts = 0
+
+while True:
+    pid = os.fork()
+    if pid == 0:
+        os.execlp(sys.argv[1], *sys.argv[1:])
+
+    #print >>sys.stderr, "relight: pid", pid
+    pid, status = os.waitpid(pid, 0)
+    exitcode = (status >> 8) & 0xff
+    signal = status & 0xff
+    if signal == 0:
+        sys.exit(exitcode)
+
+    print >>sys.stderr, "relight: process exited with signal", signal
+    log = open("relight.log", "a")
+    print >>log, time.ctime(), "exited with signal", signal
+    log.close()
+
+    minute = time.time() / 60
+    if minute != current_minute:
+        current_minute = minute
+        restarts = 0
+    restarts += 1
+    if restarts >= 5:
+        print >>sys.stderr, "relight: too many restarts, exiting"
+        sys.exit(1)
+
+    time.sleep(5)
+    print >>sys.stderr, "relight: restarting process now"
